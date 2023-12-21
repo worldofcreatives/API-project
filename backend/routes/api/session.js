31b7092294e8d54287c1b2f713004455d6979a25
worldofcreatives <worldofcreatives.com@gmail.com>
2023-12-20 19:05:48 -0500

    Phase 4.2
    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -45,4 +45,12 @@ router.post(
     }
   );
 
+  router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
+
 module.exports = router;
