9696cb231629350381f2fa196e413aa1dfe98dd2
worldofcreatives <worldofcreatives.com@gmail.com>
2023-12-21 13:35:14 -0500

    Update 20231221174840-add-name-fields-to-users.js

--- backend/db/migrations/20231221174840-add-name-fields-to-users.js
+++ backend/db/migrations/20231221174840-add-name-fields-to-users.js
@@ -1,15 +1,22 @@
 'use strict';
 
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+options.tableName = "Users";
+
 /** @type {import('sequelize-cli').Migration} */
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn("Users", "firstName", {
+    await queryInterface.addColumn(options, "firstName", {
       type: Sequelize.STRING,
       allowNull: false,
       defaultValue: "",
     });
-    await queryInterface.addColumn("Users", "lastName", {
+    await queryInterface.addColumn(options, "lastName", {
       type: Sequelize.STRING,
       allowNull: false,
       defaultValue: "",
@@ -17,8 +24,8 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn("Users", "firstName");
-    await queryInterface.removeColumn("Users", "lastName");
+    await queryInterface.removeColumn(options, "firstName");
+    await queryInterface.removeColumn(options, "lastName");
   },
 };
 
